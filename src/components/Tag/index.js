b5f0108c43d17f29b279b8d8d2042b0ee7bbf60f
Rohan negi <rohan.negi@Rohans-MacBook-Pro.local>
2023-07-05 13:49:34 +0530

    CNTO-6041: fix ux review issue

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -3,19 +3,33 @@ import './styles.scss'
 import PropTypes from 'prop-types'
 import { Text } from '../Typography/index'
 import { Icon } from '../Icon/index'
-import { Block } from '../Block'
+import { Block } from '../../components/Block/index'
 
 export const TAG_TYPES = ['success', 'danger', 'disabled', 'progress', 'default', 'warning']
 /**
  * Tag component that is used to specify status of a task
  */
-export const Tag = ({ className = '', type, label, children, icon, svg, ...props }) => {
+export const Tag = ({
+  className = '',
+  type,
+  label,
+  subType = '',
+  children,
+  icon,
+  svg,
+  ...props
+}) => {
   return (
-    <span className={['sg contacto-tag', className, `contacto-tag--${type}`].join(' ')} {...props}>
+    <span
+      className={['sg contacto-tag', className, `contacto-tag--${type}`, ` ${subType}`].join(' ')}
+      {...props}
+    >
       <Block display="flex" alignItems="center">
         {icon && <Icon name={icon} size={14} className="tag-icon" />}
         {svg && <Icon svg={svg} size={14} className="tag-icon" />}
-        <Text type={type === 'default' ? 'caption' : 'caption-capital'}>{label || children}</Text>
+        <Text type={type === 'default' || subType === 'compact' ? 'caption' : 'caption-capital'}>
+          {label || children}
+        </Text>
       </Block>
     </span>
   )
@@ -30,6 +44,10 @@ Tag.propTypes = {
    * The type of tag
    */
   type: PropTypes.oneOf(TAG_TYPES),
+  /**
+   * The sub type of tag
+   */
+  subType: PropTypes.string,
   /**
    * The text to be displayed
    */
